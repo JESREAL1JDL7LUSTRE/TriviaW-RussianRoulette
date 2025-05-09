4818f7e82cb6076d9c2c03beb23fd017152f1f94
baeeyuh <bacaling.beaclarise@gmail.com>
2025-05-09 15:37:49 +0800

    bg

--- core/src/main/java/io/github/TriviaWRussianRoulette/JESREAL1JDL7LUSTRE/FirstScreen.java
+++ core/src/main/java/io/github/TriviaWRussianRoulette/JESREAL1JDL7LUSTRE/FirstScreen.java
@@ -2,14 +2,18 @@ package io.github.TriviaWRussianRoulette.JESREAL1JDL7LUSTRE;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class FirstScreen extends BaseScreen {
+
+    private Texture bgTexture;
 
     public FirstScreen(Main game) {
         super(game);
@@ -22,12 +26,18 @@ public class FirstScreen extends BaseScreen {
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
 
-        // 2) Build a simple table layout
+        // 2) Add background
+        bgTexture = new Texture(Gdx.files.internal("Home.png"));
+        Image background = new Image(bgTexture);
+        background.setFillParent(true);
+        stage.addActor(background);
+
+        // 3) Build table layout
         Table table = new Table();
         table.setFillParent(true);
         stage.addActor(table);
 
-        // 3) Create buttons
+        // 4) Create buttons
         TextButton playBtn = new TextButton("Play", game.uiSkin);
         TextButton optionsBtn = new TextButton("Options", game.uiSkin);
         TextButton howToPlayBtn = new TextButton("How To Play", game.uiSkin);
@@ -36,12 +46,11 @@ public class FirstScreen extends BaseScreen {
         TextButton addOwnQuestionsBtn = new TextButton("Add Topic/Questions", game.uiSkin);
         TextButton addEditQuestionsBtn = new TextButton("Add Edit Questions", game.uiSkin);
 
-        // 4) Add listeners
+        // 5) Add listeners
         playBtn.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                // Switch to the gameplay screen
-                game.setScreen(new io.github.TriviaWRussianRoulette.JESREAL1JDL7LUSTRE.CustomizeGameplay(game));
+                game.setScreen(new CustomizeGameplay(game));
             }
         });
         exitBtn.addListener(new ChangeListener() {
@@ -81,25 +90,24 @@ public class FirstScreen extends BaseScreen {
             }
         });
 
-        // 5) Lay out buttons in the table
-        table.add(playBtn).width(200).height(50).pad(10);
+        // 6) Lay out buttons
+        table.add(playBtn).width(200).height(50).pad(10).padLeft(1000);
         table.row();
-        table.add(addOwnQuestionsBtn).width(200).height(50).pad(10);
+        table.add(addOwnQuestionsBtn).width(200).height(50).pad(10).padLeft(1000);
         table.row();
-        table.add(addEditQuestionsBtn).width(200).height(50).pad(10);
+        table.add(addEditQuestionsBtn).width(200).height(50).pad(10).padLeft(1000);
         table.row();
-        table.add(optionsBtn).width(200).height(50).pad(10);
+        table.add(optionsBtn).width(200).height(50).pad(10).padLeft(1000);
         table.row();
-        table.add(howToPlayBtn).width(200).height(50).pad(10);
+        table.add(howToPlayBtn).width(200).height(50).pad(10).padLeft(1000);
         table.row();
-        table.add(aboutBtn).width(200).height(50).pad(10);
+        table.add(aboutBtn).width(200).height(50).pad(10).padLeft(1000);
         table.row();
-        table.add(exitBtn).width(200).height(50).pad(10);
+        table.add(exitBtn).width(200).height(50).pad(10).padLeft(1000);
     }
 
     @Override
     public void render(float delta) {
-        // Clear and draw UI
         Gdx.gl.glClearColor(0.1f, 0.1f, 0.15f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act(delta);
@@ -113,6 +121,10 @@ public class FirstScreen extends BaseScreen {
 
     @Override
     public void dispose() {
+        super.dispose();
         stage.dispose();
+        if (bgTexture != null) {
+            bgTexture.dispose();
+        }
     }
 }
