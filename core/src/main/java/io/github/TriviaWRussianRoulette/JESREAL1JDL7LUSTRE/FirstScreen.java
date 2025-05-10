fde4cf4cd1f68fa80781318ab48785b6684908c2
Jesreal D. Lustre <115926421+JESREAL1JDL7LUSTRE@users.noreply.github.com>
2025-05-10 12:25:15 +0800

    added some fixes

--- core/src/main/java/io/github/TriviaWRussianRoulette/JESREAL1JDL7LUSTRE/FirstScreen.java
+++ core/src/main/java/io/github/TriviaWRussianRoulette/JESREAL1JDL7LUSTRE/FirstScreen.java
@@ -2,14 +2,17 @@ package io.github.TriviaWRussianRoulette.JESREAL1JDL7LUSTRE;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class FirstScreen extends BaseScreen {
+    private Texture bgTexture;
 
     public FirstScreen(Main game) {
         super(game);
@@ -21,6 +24,12 @@ public class FirstScreen extends BaseScreen {
         // 1) Create a Stage and set input to it
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
+
+        // 2) Add background
+        bgTexture = new Texture(Gdx.files.internal("Home.png"));
+        Image background = new Image(bgTexture);
+        background.setFillParent(true);
+        stage.addActor(background);
 
         // 2) Build a simple table layout
         Table table = new Table();
@@ -41,7 +50,7 @@ public class FirstScreen extends BaseScreen {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
                 // Switch to the gameplay screen
-                game.setScreen(new io.github.TriviaWRussianRoulette.JESREAL1JDL7LUSTRE.CustomizeGameplay(game));
+                game.setScreen(new CustomizeGameplay(game));
             }
         });
         exitBtn.addListener(new ChangeListener() {
@@ -82,19 +91,19 @@ public class FirstScreen extends BaseScreen {
         });
 
         // 5) Lay out buttons in the table
-        table.add(playBtn).width(200).height(50).pad(10);
+        table.add(playBtn).width(200).height(50).pad(10).padLeft(1000);
         table.row();
-        table.add(addOwnQuestionsBtn).width(200).height(50).pad(10);
+        table.add(addOwnQuestionsBtn).width(200).height(50).pad(10).padLeft(1000);
         table.row();
-        table.add(addEditQuestionsBtn).width(200).height(50).pad(10);
+        table.add(addEditQuestionsBtn).width(200).height(50).pad(10).padLeft(1000);
         table.row();
-        table.add(optionsBtn).width(200).height(50).pad(10);
+        table.add(optionsBtn).width(200).height(50).pad(10).padLeft(1000);
         table.row();
-        table.add(howToPlayBtn).width(200).height(50).pad(10);
+        table.add(howToPlayBtn).width(200).height(50).pad(10).padLeft(1000);
         table.row();
-        table.add(aboutBtn).width(200).height(50).pad(10);
+        table.add(aboutBtn).width(200).height(50).pad(10).padLeft(1000);
         table.row();
-        table.add(exitBtn).width(200).height(50).pad(10);
+        table.add(exitBtn).width(200).height(50).pad(10).padLeft(1000);
     }
 
     @Override
@@ -113,6 +122,10 @@ public class FirstScreen extends BaseScreen {
 
     @Override
     public void dispose() {
+        super.dispose();
         stage.dispose();
+        if (bgTexture != null) {
+            bgTexture.dispose();
+        }
     }
 }
