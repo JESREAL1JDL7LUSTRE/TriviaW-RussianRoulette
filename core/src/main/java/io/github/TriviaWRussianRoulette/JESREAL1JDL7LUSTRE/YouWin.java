9d7571e18c4dba09836082f726e3f5ad22057509
Jesreal D. Lustre <115926421+JESREAL1JDL7LUSTRE@users.noreply.github.com>
2025-05-10 13:42:01 +0800

    Added buttons on the YouWin Screen

--- core/src/main/java/io/github/TriviaWRussianRoulette/JESREAL1JDL7LUSTRE/YouWin.java
+++ core/src/main/java/io/github/TriviaWRussianRoulette/JESREAL1JDL7LUSTRE/YouWin.java
@@ -1,8 +1,16 @@
 package io.github.TriviaWRussianRoulette.JESREAL1JDL7LUSTRE;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 public class YouWin extends BaseScreen{
+
+
+
     public YouWin(Main game) {
         super(game);
     }
@@ -15,5 +23,30 @@ public class YouWin extends BaseScreen{
         title.setFontScale(2);
         title.setPosition(100, 300);
         stage.addActor(title);
+
+        Table table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+
+        TextButton playBtn = new TextButton("Play Again", game.uiSkin);
+        TextButton exitBtn = new TextButton("Exit", game.uiSkin);
+
+        playBtn.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                // Switch to the gameplay screen
+                game.setScreen(new CustomizeGameplay(game));
+            }
+        });
+        exitBtn.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Gdx.app.exit();
+            }
+        });
+
+        table.add(playBtn).width(200).height(50).pad(10).padLeft(1000);
+        table.row();
+        table.add(exitBtn).width(200).height(50).pad(10).padLeft(1000);
     }
 }
